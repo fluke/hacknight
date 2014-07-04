a991cbf4387d1d54a51fd28f8abd9216fd5f1feb
Kartik Luke Singh <kartikluke@gmail.com>
2014-07-04 20:00:10 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hacknight::Application.config.secret_key_base = '2e4b220968b3c3308fbb1686948ce24fde6cf44d516deab996e7013b486112f091a5a453e3c15cd2cbb9c2d103242423b3b3596f0b3d77ef9252bd9adbdfcb85'
