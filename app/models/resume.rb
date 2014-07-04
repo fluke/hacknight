09a82008a8b02d4ee222e2cdf4a44a998a188513
Varun Mayya <VarunMayya@Varuns-MacBook-Air.local>
2014-07-04 21:38:09 +0530

    added dashboard and dashboard controller and login/logout buttons

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -5,9 +5,11 @@ class Resume < ActiveRecord::Base
 	has_many :skills
 	has_many :work_experiences 
 	has_many :extra_fields
+	belongs_to :user
 	validates_presence_of :first_name, :email, :phone
 	validates :email,
             :format => {
               :with    => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i,
-              :message => "Email format is wrong." }
+              :message => "Email format is wrong.",
+              :multiline => true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hacknight::Application.routes.draw do
+  get "dashboard/index", as: "dashboard"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
