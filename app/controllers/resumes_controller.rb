2cb66033c8ab1634dcfad6029b578157e0cb8c24
Varun Mayya <VarunMayya@Varuns-MacBook-Air.local>
2014-07-05 00:06:10 +0530

    resume format

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,11 +1,82 @@
 class ResumesController < ApplicationController
-	
+  before_action :set_resume, only: [:show, :edit, :update, :destroy]
+  before_action :check_user
+  # GET /resumes
+  # GET /resumes.json
+  def index
+    @resumes = Resume.all
+  end
+
+  # GET /resumes/1
+  # GET /resumes/1.json
   def show
-  	@resume = Resume.find_by_id(params[:id])
   end
 
-
+  # GET /resumes/new
   def new
-  	@resume = Resume.new(:user => current_user)
+    @resume = Resume.new
   end
+
+  # GET /resumes/1/edit
+  def edit
+  end
+
+  # POST /resumes
+  # POST /resumes.json
+  def create
+    @resume = Resume.new(resume_params)
+
+    respond_to do |format|
+      if @resume.save
+        format.html { redirect_to @resume, notice: 'Resume was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @resume }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @resume.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /resumes/1
+  # PATCH/PUT /resumes/1.json
+  def update
+    respond_to do |format|
+      if @resume.update(resume_params)
+        format.html { redirect_to @resume, notice: 'Resume was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @resume.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /resumes/1
+  # DELETE /resumes/1.json
+  def destroy
+    @resume.destroy
+    respond_to do |format|
+      format.html { redirect_to resumes_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def check_user
+      if @resume = current_user.resume
+      else
+        flash[:notice] = "This is not yours."
+        redirect_to root_path and return
+      end
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_resume
+      @resume = Resume.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def resume_params
+      params.require(:resume).permit(:first_name, :middle_name, :last_name, :email, :phone, :website, :theme, :url, :view_count, educations_attributes: [:id, :college, :degree, :start_duration, :end_duration, :description, :_destroy], references_attributes: [:id, :first_name, :middle_name, :last_name, :linked_in_url, :twitter_url, :description, :_destroy], skills_attributes: [:id, :description, :_destroy], work_experiences_attributes: [:id, :company, :position, :start_duration, :end_duration, :description, :_destroy], extra_fields_attributes: [:id, :field_name, :description, :_destroy])
+    end
 end
